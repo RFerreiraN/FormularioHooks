edaffd9ee7bdf6626b8f9a5d2d2fe40e0c77e31a
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-25 22:01:38 +0200

    damos estilos con bootstrap

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import {useState} from 'react'
 
 export const Formulario = () => {
   return (
-    <>
-      <h1>Formulario de Contacto:</h1>
-      <hr />
-    </>
+   <form>
+  <div className="form-group">
+    <label htmlForfor="username">User Name</label>
+
+    <input type="email" 
+           className="form-control" 
+           name="username" 
+           placeholder="Enter Username"/>
+
+  </div>
+
+  <div className="form-group">
+    <label htmlForfor="password">Password</label>
+
+    <input type="password" 
+           className="form-control" 
+           id="password" 
+           placeholder="Password"/>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
   )
 }
